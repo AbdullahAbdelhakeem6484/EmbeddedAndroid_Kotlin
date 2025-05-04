3e0460abe6fc017ee698fa765e65ce0de8d085c0
abdullahabdelhakeem6484 <abdullah.abdelhakeem657@gmail.com>
2025-05-04 19:16:18 +0300

    single and multi comment

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*multiple comment*/
+/*
+* Author : Abdullah Abdelhakeem
+* Date   : 4 May 2025
+* */
+
+// this is the entry point   - Single comment
 fun main() {
+
+
     print("Hello World");
     print("Hello World");
     println("Hello World");
